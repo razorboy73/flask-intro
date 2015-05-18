5a283a668f9a1b6e679e078cbc5eb25fc1f4dbfc
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-05-18 14:08:19 -0400

    Part 3: finished auth

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 app.secret_key="secret"
 
+
+
+#login_required
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            flash("you need to login")
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/')
+@login_required
 def home():
-    return 'Hello World!'
+    return render_template("index.html")
 
 @app.route('/welcome')
+@login_required
 def welcome():
     return render_template("welcome.html")
 
@@ -20,13 +36,16 @@ def login():
             error = "Invalid Credentials"
         else:
             session['logged_in']=True
+            flash("You are logged in")
             return redirect((url_for('home')))
     return render_template("login.html", error=error)
 
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('logged_in', None)
+    flash("You are logged out")
     return redirect(url_for('welcome'))
 if __name__ == '__main__':
     app.run(debug=True)
