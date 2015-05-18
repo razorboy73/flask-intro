389f8fae6eddc474056cb5a51b91da443287adb5
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-05-18 12:57:30 -0400

    added login page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,9 +8,19 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/welcom')
+@app.route('/welcome')
 def welcome():
     return render_template("welcome.html")
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form['username'] != "admin" or request.form['password'] != "admin":
+            error = "Invalid Credentials"
+        else:
+            return redirect((url_for('home')))
+    return render_template("login.html", error=error)
 
 
 if __name__ == '__main__':
