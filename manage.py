299d7e4aafb57f287fbe5ead7b6d3cbe6a470b74
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-05-18 22:50:39 -0400

    Part 15 - added migrations

---
+++ manage.py
@@ -0,0 +1,21 @@
+__author__ = 'workhorse'
+
+from app import app,db
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+import os
+
+# config
+app.config.from_object(os.environ["APP_SETTINGS"])
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
+
+
+
+if __name__ == '__main__':
+    #app.run()
+    manager.run()
+    #manager.add_command("shell",Shell(make_context=make_shell_context()))
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 __author__ = 'workhorse'
-from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from app import db
+
 
 
 class BlogPost(db.Model):
@@ -8,6 +10,7 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title =db.Column(db.String(128), nullable=False)
     description = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
 
     def __init__(self, title, description):
         self.title = title
@@ -16,3 +19,25 @@ class BlogPost(db.Model):
 
     def __repr__(self):
         return "<title {}>".format(self.title)
+
+
+class User(db.Model):
+    __tablename__ ="users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship("BlogPost", backref="author",  lazy="dynamic")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+
+    def __repr__(self):
+        return "<title {}>".format(self.name)
+
+
+
+
